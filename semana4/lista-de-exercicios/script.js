43d923fec56443222eac22c1d360de0c773a40f4
Rafael Cardoso <rafafscardoso@gmail.com>
2020-04-07 18:26:01 -0300

    Exercício 4

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -7,9 +7,12 @@ O valor impresso no console depende do valor da cotação a ser inserido. Para t
 o valor impresso no console seria R$500.
 
 2. A função calcula qual seria o valor final para determinados investimentos tendo como parâmetros o tipo de investimento e o valor inicial.
-No caso do exemplo, o console imprimiria: 165 e TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+No caso do exemplo, o console imprimiria: 165 \n TIPO DE INVESTIMENTO INFORMADO INCORRETO!
 
 3. O programa, tendo uma array de números, cria duas novas arrays, com a primeira contendo os números pares e a segunda os ímpares.
-No console seria impresso: Quantidade total de números 14, 6 e 8.
+No console seria impresso: Quantidade total de números 14 \n 6 \n 8.
+
+4. O programa, dado uma array de números, vai encontrar qual é o maior e o menor número nela, exceto se todos os números da array
+forem negativos, porque o programa considera o menor número 0. Seria impresso no console -10 \n 1590.
 
 */

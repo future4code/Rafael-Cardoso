90452c03e7b4897655d5dca6fc2fa62643452eef
Rafael Cardoso <rafafscardoso@gmail.com>
2020-04-07 18:04:49 -0300

    Segundo exercício

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -6,4 +6,7 @@ Exercícios de leitura de código
 O valor impresso no console depende do valor da cotação a ser inserido. Para ter um exemplo, se fosse inserido uma cotação de 5, 
 o valor impresso no console seria R$500.
 
+2. A função calcula qual seria o valor final para determinados investimentos tendo como parâmetros o tipo de investimento e o valor inicial.
+No caso do exemplo, o console imprimiria 165 e TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+
 */

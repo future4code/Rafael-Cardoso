a8c07cda052defd834b303db93ec4e81f04b5cb0
Rafael Cardoso <rafafscardoso@gmail.com>
2020-04-07 18:02:21 -0300

    Primeiro exercício

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,9 @@
+/*
+
+Exercícios de leitura de código
+
+1. A função pede para o usuário preencher o valor da cotação do dólar, calcula o valor em reais de US$100 e imprime no console o mesmo.
+O valor impresso no console depende do valor da cotação a ser inserido. Para ter um exemplo, se fosse inserido uma cotação de 5, 
+o valor impresso no console seria R$500.
+
+*/

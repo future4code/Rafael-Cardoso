bbc31e36b3371a2fe3aba9ceff421b2848f6098a
Rafael Cardoso <rafafscardoso@gmail.com>
2020-06-23 15:26:41 -0300

    Exercícios obrigatórios

---
+++ semana14/aula44-packagejson/ex_3/build/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const fileName = process.argv[2];
+const newTask = process.argv[3];
+try {
+    const data = fs.readFileSync(fileName);
+    const tasks = JSON.parse(data.toString());
+    tasks.push(newTask);
+    const tasksString = JSON.stringify(tasks);
+    fs.writeFileSync(fileName, tasksString);
+    console.log('Tarefa adicionada com sucesso!');
+}
+catch (error) {
+    console.error(error);
+}
+//# sourceMappingURL=index.js.map
